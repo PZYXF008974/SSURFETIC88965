b8aaf984d718a38ef639e04d3e5d59677482da0b
PZYXF008974 <146069360+PZYXF008974@users.noreply.github.com>
2025-04-12 18:57:53 +0300

    Update testerlua.lua

--- testerlua.lua
+++ testerlua.lua
@@ -1,2 +1,2 @@
 task.wait(3)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PZYXF008974/SSURFETIC88965/refs/heads/main/asdasd"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PZYXF008974/SSURFETIC88965/refs/heads/main/asdasd.lua", true))()
